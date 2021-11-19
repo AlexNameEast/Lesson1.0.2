45ceb021e8a9d2e3a210bdabc989f159075987cf
Alex <exampl@gmail.com>
2021-11-19 22:46:18 +0200

    finish home work

--- src/main/java/ru/geekbrainsLessonOne/lesson_1/Lesson4/H_w.java
+++ src/main/java/ru/geekbrainsLessonOne/lesson_1/Lesson4/H_w.java
@@ -13,6 +13,7 @@ public class H_w {
     static char[][] field;
     static int fieldSizeX;
     static int fieldSizeY;
+    static int winKol;
 
     static void initialize(int x, int y) {
         field = new char[x][y];
@@ -40,11 +41,43 @@ public class H_w {
 
     static void aiTurn() {
         int x, y;
-        do {
-            x = random.nextInt(fieldSizeX);
-            y = random.nextInt(fieldSizeY);
-        } while (!dotEmpty(y, x));
-        field[y][x] = DOT_AI;
+        boolean flag = true;
+
+        for (int i = 0; i < fieldSizeY; i++) {
+            for (int j = 0; j < fieldSizeX; j++) {
+                if (flag && dotEmpty(i, j)) {
+                    field[i][j] = DOT_AI;
+                    if (flag && checkWinUniversal(DOT_AI, winKol)) {
+                        flag = false;
+                        break;
+                    }else field[i][j] = DOT_EMPTY;
+                }
+            }
+        }
+        if (flag) {
+            for (int i = 0; i < fieldSizeY; i++) {
+                for (int j = 0; j < fieldSizeX; j++) {
+                    if (dotEmpty(i, j)) {
+                        field[i][j] = DOT_HUMAN;
+                        if (flag && checkWinUniversal(DOT_HUMAN, winKol)) {
+                            field[i][j] = DOT_AI;
+                            flag = false;
+                            break;
+                        } else field[i][j] = DOT_EMPTY;
+                    }
+                }
+            }
+
+            if (flag) {
+                do {
+                    x = random.nextInt(fieldSizeX);
+                    y = random.nextInt(fieldSizeY);
+                } while (!dotEmpty(y, x));
+                field[y][x] = DOT_AI;
+            }
+        }
+
+
     }
 
     static boolean isCellValid(int x, int y) {
@@ -75,7 +108,7 @@ public class H_w {
 
     static boolean gameChecks(char dot, String s) {
         // Проверка победы игрока (человек или компьютер, в зависимости от типа фишки)
-        if (checkWin2(dot,4)) {
+        if (checkWinUniversal(dot, winKol)) {
             System.out.println(s);
             return true;
         }
@@ -87,19 +120,66 @@ public class H_w {
         // Продолжение игры
         return false;
     }
-    // проверка с доп параметром
-    static boolean checkWin2(char c, int kol) {
+
+    static boolean checkWinDot(char c, int x, int y, int kol) {
+        boolean f = true;
+
+        //проверка по вертикали вправо
+        for (int i = 0; i < kol; i++) {
+            if (isCellValid(x + i, y)) f = f & (field[x + i][y] == c);
+            else f = false;
+        }
+        if (f) return f;
+
+        //проверка по горизонтали вниз
+        f = true;
+        for (int i = 0; i < kol; i++) {
+            if (isCellValid(x, y + i)) f = f & (field[x][y + i] == c);
+            else f = false;
+        }
+        if (f) return f;
+
+
+        //диагональ положительная
+        f = true;
+        for (int i = 0; i < kol; i++) {
+            if (isCellValid(x + i, y + i)) f = f & (field[x + i][y + i] == c);
+            else f = false;
+        }
+        if (f) return f;
+
+        //диагональ отрицательная
+        f = true;
+        for (int i = 0; i < kol; i++) {
+            if (isCellValid(x + i, y - i)) f = f & (field[x + i][y - i] == c);
+            else f = false;
+        }
+        if (f) return f;
+
+        return false;
+
+    }
+
+    // проверка на победу универсальная
+    static boolean checkWinUniversal(char c, int kol) {
+
+        for (int i = 0; i < fieldSizeY; i++) {
+            for (int j = 0; j < fieldSizeX; j++) {
+                if (checkWinDot(c, i, j, kol)) return true;
+            }
+        }
+        return false;
+
+    }
+
+    // старый код проверки
+    /*static boolean checkWin(char c) {
         // проверка по горизонтали
         boolean f = true;
         for (int i = 0; i < fieldSizeY; i++) {
 
             f = true;
-            for (int j = 0,k=0; j < kol; j++) {
-                if(j==kol-1+k && f==false ) {
-                    f=true;
-                    j= j-kol+2+k;
-                    k++;
-                }
+            for (int j = 0; j < fieldSizeX; j++) {
                 f = f & (field[j][i] == c);
             }
             if (f) return f;
@@ -129,45 +209,7 @@ public class H_w {
         }
 
         return false;
-    }
-
-    static boolean checkWin(char c) {
-        // проверка по горизонтали
-        boolean f = true;
-        for (int i = 0; i < fieldSizeY; i++) {
-
-            f = true;
-            for (int j = 0; j < fieldSizeX; j++) {
-                f = f & (field[j][i] == c);
-            }
-            if (f) return f;
-        }
-        // проверка по вертикали
-        f = true;
-        for (int i = 0; i < fieldSizeY; i++) {
-
-            f = true;
-            for (int j = 0; j < fieldSizeX; j++) {
-                f = f & (field[i][j] == c);
-            }
-            if (f) return f;
-        }
-        //диагональ положительная
-        f = true;
-        for (int i = 0; i < fieldSizeY; i++) {
-            f = f & (field[i][i] == c);
-        }
-        if (f) return f;
-
-        //диагональ отрицательная
-        f = true;
-        for (int i = 0, j = fieldSizeX - 1; i < fieldSizeY; i++, j--) {
-                f = f & (field[i][j] == c);
-            if (f) return f;
-        }
-
-        return false;
-    }
+    }*/
 
 
     static boolean checkDraw() {
@@ -187,6 +229,8 @@ public class H_w {
             fieldSizeX = scanner.nextInt();
             fieldSizeY = scanner.nextInt();
             initialize(fieldSizeX, fieldSizeY);
+            if (fieldSizeX > 3 || fieldSizeY > 3) winKol = 4;
+            else winKol = 3;
             drawMap();
 
             boolean f = true;
